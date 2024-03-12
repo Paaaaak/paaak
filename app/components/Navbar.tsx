c4558bff73da49fc75472e0fde2459b7a4be1378
JaehyeonPaak <wogus392@gmail.com>
2024-03-12 17:23:18 +0900

    add red theme

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
         <div>
             <nav className="w-full relative flex items-center justify-between max-w-2xl mx-auto px-4 py-5">
                 <Link href="/" className="font-bold text-3xl">
-                    Jaehyeon<span className="text-blue-500">Paak</span>
+                    Jaehyeon<span className="text-primary">Paak</span>
                 </Link>
             </nav>
         </div>
