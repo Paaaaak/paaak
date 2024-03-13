67c97b3336b4cd055b75346a8d052a289d7bf2b0
JaehyeonPaak <wogus392@gmail.com>
2024-03-13 17:49:22 +0900

    add single post template

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,7 +1,21 @@
-async function getData() {
-    
-}
+import { client, urlFor } from "@/app/lib/sanity";
 
-export default function BlogArticle({params}: {params: {slug: string}}) {
-    return <h1>{params.slug}</h1>
+async function getData(slug: string) {
+    const query = `
+      * [_type == 'blog' && slug.current == '${slug}'] | order(_createdAt asc) {
+        "currentSlug": slug.current,
+        title,
+        content,
+        postImage
+      }`;
+  
+      const data = await client.fetch(query);
+      return data;
+  }
+
+export default async function BlogArticle({params}: {params: {slug: string}}) {
+    const data = await getData(params.slug);
+    console.log(data);
+
+    return <h1>{params.slug}</h1>;
 }
