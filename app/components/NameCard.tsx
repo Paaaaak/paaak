275af27621092f6c22d2b23369e3b79a56ae60c9
JaehyeonPaak <wogus392@gmail.com>
2024-03-15 16:22:17 +0900

    add namecard

---
+++ app/components/NameCard.tsx
@@ -0,0 +1,9 @@
+export default function NameCard() {
+    return (
+      <div className="">
+        Jaehyeon Park.
+        I deliver useful Frontend and UX tips and maintain this blog as a
+        reminder to myself.
+    </div>
+    )
+}
